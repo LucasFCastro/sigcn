350d5e895aef0f2ef5c87d5f6fb70302c6c7b463
LucasFCastro <lucasfcastro@yahoo.com.br>
2017-03-03 21:45:59 -0300

    Paginate ok

--- src/components/dashboard/DataTable.vue
+++ src/components/dashboard/DataTable.vue
@@ -17,18 +17,20 @@
     </tbody>
   </table>
   <div class="text-center">
-    <ul class="pagination">
-      <li><a href="" @click.prevent='changePage(1)'>1</a></li>
+    <ul class="pagination" v-show='lastPage > 1'>
+      <li :class="{'active': 1 == currentPage}"><a href="" @click.prevent='changePage(1)'>1</a></li>
       <li v-if='pontosInicio' class="disabled">
         <a href="#">...</a>
       </li>
-      <li v-for='n in 5' :class="{'active': (toPage + n - 1) == currentPage}">
-        <a href="" @click.prevent='changePage(toPage + n - 1)'>{{toPage + n - 1}}</a>
+      <li v-for='n in showNumberPages' :class="{'active': n == currentPage}" v-show='lastPage > 2'>
+        <a href="" @click.prevent='changePage(n)'>{{n}}</a>
       </li>
       <li v-if='pontosFinal' class="disabled">
         <a href="#">...</a>
       </li>
-      <li><a href="" @click.prevent='changePage(lastPage)'>{{lastPage}}</a></li>
+      <li :class="{'active': lastPage == currentPage}">
+        <a href="" @click.prevent='changePage(lastPage)' >{{lastPage}}</a>
+      </li>
     </ul>
   </div>
 </div>
@@ -48,7 +50,7 @@ export default {
   },
   data () {
     return {
-      currentPage: 5
+      currentPage: 1
     }
   },
   computed:{
@@ -59,22 +61,41 @@ export default {
       return Math.ceil(this.total / this.perPage)
     },
     dadoTo () {
-      return (this.currentPage - 1) * this.perPage
+      return this.lastPage > 1 ? (this.currentPage - 1) * this.perPage : 0
     },
     dadosPage () {
       return this.dados.slice(this.dadoTo, this.dadoTo + this.perPage)
     },
     pontosInicio () {
-      return (this.currentPage > 7) && (this.lastPage > 9)
-    },
-    toPage () {
-      return this.currentPage > 7 ? this.currentPage - 2 : 2
-    },
-    fromPage () {
-      return this.toPage + 4
+      return (this.currentPage > 4) && (this.lastPage > 7)
     },
     pontosFinal () {
-      return (this.lastPage - this.fromPage) > 1
+      return ((this.lastPage - this.currentPage) > 4) && (this.lastPage > 7)
+    },
+    showNumberPages () {
+      let result = []
+      let inicio = 2
+      let fim = this.lastPage - 1
+      if (this.lastPage > 7) {
+        if (this.pontosInicio) {
+          if (this.lastPage - this.currentPage < 5) {
+            inicio = this.lastPage - 4
+          } else {
+            inicio = this.currentPage - 1
+          }
+        }
+        if (this.pontosFinal) {
+          if (this.currentPage < 5) {
+            fim = 5
+          } else {
+            fim = this.currentPage + 1
+          }
+        }
+      }
+      for (var i = inicio; i <= fim; i++) {
+        result.push(i)
+      }
+      return result
     }
   }
 }
